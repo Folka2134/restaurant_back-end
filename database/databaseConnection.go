46cd500414bde437333f88936d27c6eac63e3780
folka2134 <folka2134@gmail.com>
2024-02-11 10:58:40 +0000

    create database connection

---
+++ database/databaseConnection.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBInstance() *mongo.Client {
+	MongoDb := "mongodb://localhost:8080"
+	fmt.Print(MongoDb)
+
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(MongoDb))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	return client
+}
+
+var Client *mongo.Client = DBInstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName)
+	return collection
+}
